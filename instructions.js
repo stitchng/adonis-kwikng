ace2ffbdff4c633f83ea58355a40941237a31820
StitchNG <stitchnig@gmail.com>
2019-08-20 10:20:36 +0100

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -5,14 +5,16 @@
  *
  * @license MIT
  * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
- * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com> | Aziz Abdul <>
+ * @extended Oparand - Ifeora Okechukwu <isocroft@gmail.com>
  */
 
 const path = require('path')
 
 module.exports = async function (cli) {
-  await cli.makeConfig('maxdotng.js', path.join(__dirname, 'config/maxdotng.js'))
-    .catch((e) => {})
-
-  cli.command.completed('create', 'config/maxdotng.js')
+  try {
+    await cli.makeConfig('maxdotng.js', path.join(__dirname, 'config/maxdotng.js'))
+    cli.command.completed('create', 'config/maxdotng.js')
+  } catch (error) {
+    // ignore if maxdotng.js already exists
+  }
 }
